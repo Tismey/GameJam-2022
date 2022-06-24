070e39349d0a98af2c083f6fd48963f00ab21a1b
MiCad0 <denislinde4@gmail.com>
2022-06-24 11:21:24 +0200

    les mouvements du perso et de la camera avec ton shake de mort

---
+++ GAME JAM 2022/Assets/mouselook.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+    public float Mousesensitivity = 100f;
+    public Transform Playerbody;
+    float xRotation = 0f;
+    float camerashake = 0f;
+    float shakeangle = 2f;
+    public float speedmultiplier = 2f;
+    float speedshake = 5f;
+    public GameObject chr;
+    public float speedset = 3f;
+    public movements chsp;
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        chsp = chr.GetComponent<movements>();
+
+        speedshake = chsp.speed * speedmultiplier / speedset;
+        shakeangle = chsp.speed / speedmultiplier;
+
+
+        float x = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            camerashake = camerashake + speedshake * Time.deltaTime;
+
+        }
+        else
+        {
+            camerashake = Mathf.PI / 2;
+
+        }
+
+        if (camerashake > 100)
+        {
+            camerashake = 0f;
+
+
+        }
+        float mouseX = Input.GetAxis("Mouse X") * Mousesensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * Mousesensitivity * Time.deltaTime;
+
+        Playerbody.Rotate(Vector3.up * mouseX);
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation -= mouseY;
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, Mathf.Cos(camerashake) * shakeangle);
+
+    }
+}
---
+++ GAME JAM 2022/Assets/movements.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movements : MonoBehaviour
+{
+    public CharacterController CCPerso;
+    public Vector3 VMove;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VMove = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            VMove = VMove + transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            VMove = VMove - transform.right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            VMove = VMove - transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            VMove = VMove + transform.right;
+        }
+        CCPerso.Move(VMove*speed*Time.deltaTime);
+    }
+
+    
+}
