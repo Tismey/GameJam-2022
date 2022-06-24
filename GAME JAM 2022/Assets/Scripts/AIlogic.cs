33f5e8bdabff844a8b6670bfa4208f5dc6b03e4a
Tismey <108024662+Tismey@users.noreply.github.com>
2022-06-24 20:35:19 +0200

    IA commencer

---
+++ GAME JAM 2022/Assets/Scripts/AIlogic.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIlogic : MonoBehaviour
+{
+    public NavMeshAgent ag;
+    public float speed;
+    public float AttackDistance;
+    public float reactionTime;
+    public float CreactionTime = 3;
+    Vector3 Point;
+    public bool isranged;
+    GameObject projectile;
+    bool ispatrolling = true;
+    public bool stop = true;
+    bool isAlert = false;
+    Vector3[] posOfIntresse;
+    public aiview view;
+    public GameObject pl;
+
+    public float CAttackCooldown;
+    float AttackCoolDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Point = transform.position;
+        pl = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AttackCoolDown > -10)
+        {
+            AttackCoolDown -= Time.deltaTime;
+        }
+
+
+        if (ispatrolling)
+        {
+            Patrol(posOfIntresse);
+            if(Random.Range(0,50) == 0 && stop == false)
+            {
+                Stop();
+            }
+        }
+
+        if (isAlert)
+        {
+            Alert();
+        }
+        if (stop)
+        {
+            ag.SetDestination(transform.position);
+        }
+        else
+        {
+            ag.SetDestination(Point);
+        }
+
+
+       
+    }
+
+    void Patrol(Vector3[] fallBackPoints)
+    {
+        if(Vector3.Distance(Point, transform.position) < 10f)
+        {
+            Point = new Vector3(transform.position.x + Random.Range(-100,100), transform.position.y, transform.position.z + Random.Range(-100, 100));
+            if (SetDestination(Point))
+            {
+                return;
+            }
+            else
+            {
+                Point = fallBackPoints[Random.Range(0, fallBackPoints.Length)];
+            }
+        }
+    }
+
+    void Curious()
+    {
+
+    }
+
+    void Alert()
+    {
+        Point = pl.transform.position;
+        if (Vector3.Distance(pl.transform.position, transform.position) < AttackDistance && AttackCoolDown < 0)
+        {
+            Stop();
+            Attack(pl.transform.position);
+            AttackCoolDown = CAttackCooldown;
+        }
+    }
+
+    void Attack(Vector3 target)
+    {
+
+    }
+    private bool SetDestination(Vector3 targetDestination)
+    {
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(targetDestination, out hit, 1f, NavMesh.AllAreas))
+        {
+
+            return true;
+        }
+        return false;
+    }
+
+
+    void Think()
+    {
+        if(view.insight == true)
+        {
+            reactionTime -= Time.deltaTime;
+        }
+        else
+        {
+            reactionTime = CreactionTime;
+        }
+
+        if(reactionTime < 0)
+        {
+            isAlert = true;
+            ispatrolling = false;
+        }
+    }
+
+    void Stop()
+    {
+        stop = true;
+        StartCoroutine("cooldown", Random.Range(5, 10));
+    }
+    IEnumerator cooldown(float time)
+    {
+        yield return new WaitForSeconds(time);
+        stop = false;
+    }
+}
---
+++ GAME JAM 2022/Assets/Scripts/aiview.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aiview : MonoBehaviour
+{
+    public float distance = 10;
+    public float angle = 30;
+    public float height = 1.0f;
+    public Color colormesh = Color.red;
+    Mesh mesh;
+
+    public float effectivesppoting;
+    public bool armored;
+    public bool hasat;
+
+
+    Collider count;
+    
+    public int scanfreq = 2;
+    float scaninterval;
+    float scantimer;
+
+    public LayerMask layers;
+    public LayerMask occlusionlayer;
+    public LayerMask teamcolor;
+
+    Collider[] colliders = new Collider[50];
+    public GameObject pl;
+    public bool insight = false;
+
+    
+
+    void Start()
+    {
+        scaninterval = 1.0f / scanfreq;
+       
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        scantimer += 1 * Time.deltaTime;
+        if(scantimer >= scaninterval)
+        {
+          Scan();
+            scantimer = 0;
+        }
+       
+        
+        
+    }
+
+    private void Scan()
+    {
+        if(Physics.CheckSphere(transform.position, distance, layers, QueryTriggerInteraction.Collide)){
+            
+            if (Insight(pl))
+            {
+                insight = true;
+                
+            }
+            else
+            {
+                insight = false;
+            }
+        }
+    }
+    public bool Insight(GameObject obj)
+    {
+        Vector3 origin = transform.position;
+        Vector3 dist = obj.transform.position;
+        Vector3 direction = dist - origin;
+
+
+
+        if (obj.layer == gameObject.layer)
+        {
+            return false;
+        }
+        if (obj.layer != 10)
+        {
+            return false;
+        }
+
+
+        if (direction.y < -height || direction.y > height)
+        {
+            return false;
+        }
+        direction.y = 0;
+        float deltaAngle = Vector3.Angle(direction, transform.forward);
+
+        if (deltaAngle > angle)
+        {
+            return false;
+        }
+
+        if (Physics.Linecast(origin, dist, occlusionlayer ))
+        {
+            return false;
+        }
+     
+
+        return true;
+      
+    }
+
+    Mesh createwedgemesh()
+    {
+        Mesh mesh = new Mesh();
+        int numtriangle = 8;
+        int numvertices = numtriangle * 3;
+
+        Vector3[] vertices = new Vector3[numvertices];
+        int[] triangles = new int[numvertices];
+
+        Vector3 bottomcenter = Vector3.zero;
+        Vector3 bottomleft = Quaternion.Euler(0, -angle, 0) * Vector3.forward * distance;
+        Vector3 bottomright = Quaternion.Euler(0, angle, 0) * Vector3.forward * distance;
+
+        Vector3 topcenter = bottomcenter + Vector3.up * height;
+        Vector3 topright = bottomright + Vector3.up * height;
+        Vector3 topleft = bottomleft + Vector3.up * height;
+
+        int verts = 0;
+        //left side
+        vertices[verts++] = bottomcenter;
+        vertices[verts++] = bottomleft;
+        vertices[verts++] = topleft;
+
+        vertices[verts++] = topleft;
+        vertices[verts++] = topcenter;   
+        vertices[verts++] = bottomcenter;
+
+
+        //rightside
+        vertices[verts++] = bottomcenter;
+        vertices[verts++] = topcenter;
+        vertices[verts++] = topright;
+
+
+        vertices[verts++] = topright;
+        vertices[verts++] = bottomright;
+        vertices[verts++] = bottomcenter;
+
+        //farside
+        vertices[verts++] = bottomleft;
+        vertices[verts++] = bottomright;
+        vertices[verts++] = topright;
+
+
+        vertices[verts++] = topright;
+        vertices[verts++] = topleft;
+        vertices[verts++] = bottomleft;
+
+        //top side
+        vertices[verts++] = topcenter;
+        vertices[verts++] = topleft;
+        vertices[verts++] = topright;
+
+         //bottom side
+        vertices[verts++] = bottomcenter;
+        vertices[verts++] = bottomright;
+        vertices[verts++] = bottomleft;
+
+        for(int i = 0;i < numvertices; ++i)
+        {
+            triangles[i] = i;
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.RecalculateNormals();
+        }
+
+
+
+
+        return mesh;
+    }
+
+    private void OnValidate()
+    {
+        mesh = createwedgemesh();
+        scaninterval = 1.0f / scanfreq;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (mesh)
+        {
+            Gizmos.color = colormesh;
+            Gizmos.DrawMesh(mesh, transform.position, transform.rotation);
+        }
+        
+      
+    }
+}
