2c5e2cd5fea9de04bc8c7d711c4a32ff0025d16d
MiCad0 <denislinde4@gmail.com>
2022-06-26 11:12:21 +0200

    Ajout du don du sang + painmeter

--- GAME JAM 2022/Assets/Scripts/Attack.cs
+++ GAME JAM 2022/Assets/Scripts/Attack.cs
@@ -6,20 +6,33 @@ public class Attack : MonoBehaviour
 {
     public PlayerHealth playerStats;
     public AIlogic ai;
+
+    private void Start()
+    {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
     private void OnTriggerStay(Collider other)
-    { 
-        if (other.GetComponent<AIlogic>() != null && gameObject.GetComponentInParent<movements>() != null)
+    {
+
+        if (Input.GetMouseButtonDown(0))
         {
-            
-            if (Input.GetMouseButtonDown(0))
+            if (other.GetComponent<AIlogic>() != null && gameObject.GetComponentInParent<movements>() != null)
             {
                 other.GetComponent<AIlogic>().health -= 100;
                 
                 playerStats.GiveHealth(1000);
             }
+            else if (other.tag == "barrel")
+            {
+                Debug.Log("cheh");
+                playerStats.TakeDamage(500);
+            }
         }
-        else if(other.GetComponent<movements>() != null  && ai.AttackCoolDown <0){
-                    playerStats.TakeDamage(1000);
+
+        else if (other.GetComponent<movements>() != null && ai.AttackCoolDown < 0)
+        {
+            playerStats.TakeDamage(1000);
             ai.AttackCoolDown = ai.CAttackCooldown;
         }
     }
--- GAME JAM 2022/Assets/Scripts/PlayerHealth.cs
+++ GAME JAM 2022/Assets/Scripts/PlayerHealth.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int health;
+    public int health = 6000;
+    public int pain = 20000;
+    public int bloodGiven;
 
     // Update is called once per frame
     void Update()
     {
-        if (health > 0)
+        if (pain > 0)
         {
-            --health;
+            --pain;
         }
         else
         {
@@ -24,6 +26,7 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+        pain += damage;
     }
 
     public void GiveHealth(int amount)
